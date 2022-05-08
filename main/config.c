e3ec014737b6931c6c5dee91dd9bedb0b67d3db3
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2022-05-08 15:59:17 +0500

    Features/[bind,unbind]

--- main/config.c
+++ main/config.c
@@ -3,60 +3,92 @@
 #include "estr.h"
 #include "config.h"
 
-#define NUM_APPLIANCES 10
-
 void config_init(struct Config *config) {
+    int validPins[10] = {1, 2, 3, 4, 5, 6};
+    config->maxAppliances = 10; 
 
-    for (int i = 0; i < NUM_APPLIANCES; i++) config->appliances[i] = NULL;
-    config->pinsUsed = 0;
-}
+    for (int i = 0; i < config->maxAppliances; i++) {
+        config->validPins[i] = validPins[i];
+    }
 
-void config_delete(struct Config *config) {
-    for (int i = 0; i < NUM_APPLIANCES; i++) if (config->appliances[i]) free(config->appliances[i]);
-}
-
-void register_appliance(struct Config *config, char *name) {
-    for (int i = 0; i < NUM_APPLIANCES; i++) {
-        if (!config->appliances[i]) {
-            config->appliances[i] = estr_cat(name);
-            config->pinsUsed++;
-            return;
-        }
+    for (int i = 0; i < config->maxAppliances; i++) {
+        config->appliances[i] = NULL;
     }
 }
 
-void delete_appliance(struct Config *config, char *name) {
-    for (int i = 0; i < NUM_APPLIANCES; i++) {
+void config_delete(struct Config *config) {
+    for (int i = 0; i < config->maxAppliances; i++) if (config->appliances[i]) free(config->appliances[i]);
+}
+
+int register_appliance(struct Config *config, char *name) {
+    // if no pins are vacant, return err_code: -1
+    if (config->pinsUsed == config->maxAppliances) return -1;
+    // If Appliance Already Exists, return err_code: -2 indicating that appliance already exists
+    for (int i = 0; i < config->maxAppliances; i++) {
+        if (estr_eq(config->appliances[i], name)) return -2;
+    }
+
+    // If Appliance Already Does not Exist find a vacant pin and create appliance
+    for (int i = 0; i < config->maxAppliances; i++) {
+        int pin = config->validPins[i];
+
+        if (!config->appliances[i]) {
+            config->appliances[i] = estr_cat(name);
+            config->pinsUsed++;
+            return pin;
+        }
+    }
+
+    // if no pins are vacant, return err_code: -1
+    return -1;
+}
+
+int delete_appliance(struct Config *config, char *name) {
+    if (config->pinsUsed == 0) return -1;
+
+    // find the index of the appliance and remove appliance
+    for (int i = 0; i < config->maxAppliances; i++) {
         if (config->appliances[i] && estr_eq(config->appliances[i], name)) {
             free(config->appliances[i]);
             config->appliances[i] = NULL;
             config->pinsUsed--;
+
+            return config->validPins[i];
         }
     }
+
+    // if appliance does not exist, return err_code: -1
+    return -1;
 }
 
 char *parse_and_execute_commands(struct Config *config, char *command) {
     if (command[0] != '!') return NULL;
 
     char buffer[100];
+    char *response = NULL;
+    char pinString[10];
 
-    if (estr_sw(command, "!register ")) {
-        getWord(command, 10, buffer, 100);
-        register_appliance(config, buffer);
-        char *response = estr_cat("Registered Appliance, `", buffer, "` on Pin 1");
-        return response;
+    if (estr_sw(command, "!bind ")) {
+        getNextWord(command, 6, buffer, 100);
+        int pin = register_appliance(config, buffer);
+        itoa(pin,pinString,10); 
 
-    } else if (estr_sw(command, "!drop ")) {
-        getWord(command, 6, buffer, 100);
-        delete_appliance(config, buffer);
-        char *response = estr_cat("Dropped Appliance, `", buffer, "` from Pin 1");
-        return response;
+        if (pin > 0) response = estr_cat("Bound `", buffer, "` to Pin ", pinString);
+        else if (pin == -1) response = estr_cat("All Pins Used. Kindly unbind a pin before trying again");
+        else if (pin == -2) response = estr_cat("`", buffer, "` already bound to Pin `", pinString);
+    } else if (estr_sw(command, "!unbind ")) {
+        getNextWord(command, 8, buffer, 100);
+        int pin = delete_appliance(config, buffer);
+        itoa(pin,pinString,10); 
+
+        if (pin > 0)  response = estr_cat("Unbound `", buffer, "` from Pin ", pinString);
+        else if (pin == -1) response = estr_cat("`", buffer, "`is not bound to any pin!");
     }
 
-    return NULL;
+    return response;
 }
 
-void getWord(const char *command, int startIndex, char *buffer, int bufferSize) {
+int getNextWord(const char *command, int startIndex, char *buffer, int bufferSize) {
     int index = 0;
     char letter = command[startIndex];
 
@@ -66,6 +98,5 @@ void getWord(const char *command, int startIndex, char *buffer, int bufferSize) 
     }
     
     buffer[index] = '\0';
-    
-    return buffer;
+    return index;
 }
--- main/config.h
+++ main/config.h
@@ -1,15 +1,17 @@
 #pragma once
 
 struct Config {
-    int pinsUsed;
     char* appliances[10];
+    int validPins[10];
+    int maxAppliances;
+    int pinsUsed;
 };
 
 typedef struct Config Config;
 
 void config_init(struct Config*);
 void config_delete(struct Config*);
-void register_appliance(struct Config *config, char *name);
-void delete_appliance(struct Config *config, char *name);
+int register_appliance(struct Config *config, char *name);
+int delete_appliance(struct Config *config, char *name);
 char *parse_and_execute_commands(struct Config *config, char *command);
-void getWord(const char *command, int startIndex, char *buffer, int bufferSize);
+int getNextWord(const char *command, int startIndex, char *buffer, int bufferSize);
